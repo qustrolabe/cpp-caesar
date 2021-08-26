1403aee96de1082501941ffc17c3b36c56ac079f
Nathman999 <nathman@gmail.com>
2021-08-26 21:46:16 +0300

    Changed way of reading stdin

--- main.cpp
+++ main.cpp
@@ -6,39 +6,38 @@ using std::cin;
 using std::cout;
 using std::string;
 
-unsigned shift_num(unsigned num, unsigned alphabet_size, int delta)
-{
-    return (num + delta) % alphabet_size;
+unsigned shift_num(unsigned num, unsigned alphabet_size, int delta) {
+  return (num + delta) % alphabet_size;
 }
 
-std::string ceasar(std::string text, std::string alphabet, int delta)
-{
-    int pos;
-    for (auto &c : text)
-    {
-        if ((pos = (int)alphabet.find(c)) == -1) continue;
-        c = alphabet[shift_num(pos, alphabet.size(), delta)];
-    }
-    return text;
+std::string ceasar(std::string text, std::string alphabet, int delta) {
+  int pos;
+  for (auto &c : text) {
+    if ((pos = (int)alphabet.find(c)) == -1)
+      continue;
+    c = alphabet[shift_num(pos, alphabet.size(), delta)];
+  }
+  return text;
 }
 
-std::string ceasar_encode(std::string text, std::string alphabet, int delta = 3){
-  return ceasar(text,alphabet,delta);
+std::string ceasar_encode(std::string text, std::string alphabet,
+                          int delta = 3) {
+  return ceasar(text, alphabet, delta);
 }
 
-std::string ceasar_decode(std::string text, std::string alphabet, int delta = 3){
-  return ceasar(text,alphabet,-delta);
+std::string ceasar_decode(std::string text, std::string alphabet,
+                          int delta = 3) {
+  return ceasar(text, alphabet, -delta);
 }
 
-int main(int argc, char const *argv[])
-{
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    unsigned delta = 3;
+int main(int argc, char const *argv[]) {
+  std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  unsigned delta = 3;
 
-    std::string text;
-    std::cin>>text;
-    std::cout << text << "\n";
-    std::cout << ceasar_encode(text, alphabet) << "\n";
+  // Read and encode line by line
+  for (std::string line; std::getline(std::cin, line);) {
+   std::cout << ceasar_encode(line, alphabet) << "\n";
+  }
 
-    return 0;
+  return 0;
 }
