7bcacd09e49d4b937f02b712d81e1ccd977b81f4
nathman <nathman@gmail.com>
2021-08-26 17:57:57 +0300

    Шифрование из stdin с стандартным смещением

--- main.cpp
+++ main.cpp
@@ -2,22 +2,36 @@
 #include <string>
 #include <unistd.h>
 
-int main(int argc, char const *argv[]) {
-  char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                     'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                     'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-  unsigned delta = 3;
-  std::string text = "Hello World";
+using std::cin;
+using std::cout;
+using std::string;
 
-  signed int fd[2];
-  static char buf[50];
+unsigned shift_num(unsigned num, unsigned alphabet_size, unsigned delta)
+{
+    return (num + delta) % alphabet_size;
+}
 
-  std::string lineInput;
-  while (std::cin >> lineInput) {
-	  std::cout << lineInput;
-  }
+std::string ceasar(std::string text, std::string alphabet, unsigned delta)
+{
+    int pos;
+    for (auto &c : text)
+    {
+        if ((pos = (int)alphabet.find(c)) == -1) continue;
+        c = alphabet[shift_num(pos, alphabet.size(), delta)];
+    }
+    return text;
+}
 
-  return 0;
+int main(int argc, char const *argv[])
+{
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    unsigned delta = 3;
+
+    std::string text;
+    // text =  = "Hello World123Hello 123 Hell ABC sdq";
+    std::cin>>text;
+    std::cout << text << "\n";
+    std::cout << ceasar(text, alphabet, delta) << "\n";
+
+    return 0;
 }
